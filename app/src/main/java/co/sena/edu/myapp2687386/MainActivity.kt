3e70fd5677590e2fa9d1e7f0f3a9d5e3fa826dc1
unknown <Aprendiz@BOGDFPCGMF4097.FormacionSena.RED>
2023-08-15 11:58:23 -0500

    avances de el proyecto

--- app/src/main/java/co/sena/edu/myapp2687386/MainActivity.kt
+++ app/src/main/java/co/sena/edu/myapp2687386/MainActivity.kt
@@ -2,34 +2,18 @@ package co.sena.edu.myapp2687386
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.sena.edu.myapp2687386.ui.theme.MyApp2687386Theme
@@ -38,71 +22,52 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-         MyApp2687386Theme( darkTheme = isSystemInDarkTheme()) {
-            Surface(modifier = Modifier.fillMaxSize(),color = MaterialTheme.colorScheme.scrim) {
-                 Conversation(SampleData.conversationSample)
-            }
-         }
-        }
-    }
-}
-data class Message(val author: String , val  body : String)
-
-@Composable
-fun MessageCard( msg: Message){
-
-        Row (
-            modifier=Modifier.padding(all=8.dp)) {
-            Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "Contact profile picture",
-                modifier= Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-
-            )
-            Spacer(modifier = Modifier.height(50.dp))
-            // state verification
-            var isExpanded by remember { mutableStateOf(false) }
-
-            Column(modifier = Modifier.clickable { isExpanded = !isExpanded } )     {
-
-                Text(text = msg.author, color= MaterialTheme.colorScheme.scrim, style = MaterialTheme.typography.titleSmall)
-                Spacer(modifier = Modifier.height(4.dp))
-
-                Surface(shape=MaterialTheme.shapes.medium,shadowElevation = 4.dp, color = MaterialTheme.colorScheme.scrim, border = BorderStroke(2.dp, color = Color.Green)) {
-
-                    Text(text = msg.body,modifier=Modifier.padding(all=4.dp) , style = MaterialTheme.typography.bodyLarge ,maxLines = if( isExpanded)Int.MAX_VALUE else 1, color = Color.White)
-
+            MyApp2687386Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
                 }
-
             }
         }
-
-
-
-}
-
-
-
-@Preview(showBackground = true )
-@Composable
-fun MessageCardPreview(){
-    MyApp2687386Theme {
-        Surface(color = MaterialTheme.colorScheme.scrim  ,  ) {
-            MessageCard (msg = Message("Nacho","la vida es muy corta como para morir virgen "))
-         }
-
     }
 }
 
+
 @Composable
-fun Conversation(messages:List<Message>){
-    LazyColumn{
-        items(messages){
-            message -> MessageCard(message)
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier) {
+        for (name in names) {
+            Greeting(name = name)
         }
-
     }
 }
+
+
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello,")
+            Text(text = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    MyApp2687386Theme {
+        MyApp()
+    }
+}
+
 
 
 
